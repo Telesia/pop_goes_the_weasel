70bb4f94dde03c9f2919d7f0e1dea5a1ef2110e7
Telesia <sarahtelesia@gmail.com>
2021-03-25 13:55:10 +0000

    Add sign in template and GET method to return template in app

--- app.py
+++ app.py
@@ -52,6 +52,11 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/sign_in", methods=["GET", "POST"])
+def sign_in():
+    return render_template("sign_in.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
